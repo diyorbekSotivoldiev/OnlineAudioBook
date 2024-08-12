c92f59b582496aced7960afc1cba2d66ae2af687
diyorbekSotivoldiev <sotivoldievdiyorbek@gmail.com>
2024-08-12 17:57:15 +0500

    in creating process1

---
+++ src/main/java/org/example/onlineaudiobook/AudioService.java
@@ -0,0 +1,17 @@
+package org.example.onlineaudiobook;
+
+import lombok.RequiredArgsConstructor;
+import org.example.onlineaudiobook.entity.Audio;
+import org.example.onlineaudiobook.entity.Book;
+import org.example.onlineaudiobook.repository.AudioRepository;
+import org.example.onlineaudiobook.repository.BookRepository;
+import org.hibernate.Hibernate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AudioService {
+
+
+}
--- src/main/java/org/example/onlineaudiobook/OnlineAudioBookApplication.java
+++ src/main/java/org/example/onlineaudiobook/OnlineAudioBookApplication.java
@@ -1,17 +1,7 @@
 package org.example.onlineaudiobook;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-import javax.sound.sampled.*;
-import javax.swing.*;
-import java.awt.*;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.util.Scanner;
 
 @SpringBootApplication
 
